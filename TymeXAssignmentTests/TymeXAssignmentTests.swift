2b2abb85ada125510091576a035c0f6c0d287220
thanhhvt7196 <thanhhvt7196@gmail.com>
2025-06-02 20:03:39 +0700

    Initial Commit

---
+++ TymeXAssignmentTests/TymeXAssignmentTests.swift
@@ -0,0 +1,17 @@
+//
+//  TymeXAssignmentTests.swift
+//  TymeXAssignmentTests
+//
+//  Created by thanh tien on 2/6/25.
+//
+
+import Testing
+@testable import TymeXAssignment
+
+struct TymeXAssignmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
