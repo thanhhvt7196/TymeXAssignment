d3e1da3c180271dd53870806ce09d5c849b16e00
thanhhvt7196 <thanhhvt7196@gmail.com>
2025-06-02 20:35:05 +0700

    Add Fonts, String files and Assets

---
+++ TymeXAssignment/Resources/Generated/Strings.swift
@@ -0,0 +1,40 @@
+// swiftlint:disable all
+// Generated using SwiftGen — https://github.com/SwiftGen/SwiftGen
+
+import Foundation
+
+// swiftlint:disable superfluous_disable_command file_length implicit_return prefer_self_in_static_references
+
+// MARK: - Strings
+
+// swiftlint:disable explicit_type_interface function_parameter_count identifier_name line_length
+// swiftlint:disable nesting type_body_length type_name vertical_whitespace_opening_braces
+internal enum L10n {
+  /// Chi tiết người dùng
+  internal static let userDetailScreenTitle = L10n.tr("Localizable", "user_detail_screen_title", fallback: "Chi tiết người dùng")
+  /// Người dùng Github
+  internal static let userListScreenTitle = L10n.tr("Localizable", "user_list_screen_title", fallback: "Người dùng Github")
+}
+// swiftlint:enable explicit_type_interface function_parameter_count identifier_name line_length
+// swiftlint:enable nesting type_body_length type_name vertical_whitespace_opening_braces
+
+// MARK: - Implementation Details
+
+extension L10n {
+  private static func tr(_ table: String, _ key: String, _ args: CVarArg..., fallback value: String) -> String {
+    let format = BundleToken.bundle.localizedString(forKey: key, value: value, table: table)
+    return String(format: format, locale: Locale.current, arguments: args)
+  }
+}
+
+// swiftlint:disable convenience_type
+private final class BundleToken {
+  static let bundle: Bundle = {
+    #if SWIFT_PACKAGE
+    return Bundle.module
+    #else
+    return Bundle(for: BundleToken.self)
+    #endif
+  }()
+}
+// swiftlint:enable convenience_type
