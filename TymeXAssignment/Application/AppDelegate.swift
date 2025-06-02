5dab6d79912510968cb8cd0385696cb5942f11a3
thanhhvt7196 <thanhhvt7196@gmail.com>
2025-06-02 20:49:26 +0700

    add AppDelegate

---
+++ TymeXAssignment/Application/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  TymeXAssignment
+//
+//  Created by thanh tien on 2/6/25.
+//
+
+import UIKit
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        return true
+    }
+}
